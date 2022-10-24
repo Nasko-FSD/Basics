7fe313ab024f29f6c513ce50c10eb25f577a4da1
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2022-10-24 21:24:18 +0300

    WebSumator

---
+++ HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSumator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calculate(decimal num1, decimal num2)
+        {
+            this.ViewBag.num1 = num1;
+            this.ViewBag.num2 = num2;
+            var sum = num1 + num2;
+            this.ViewBag.Sum = sum;
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
