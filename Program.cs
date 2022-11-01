ad2f493825311c16ad604efdf43314af345b4705
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2022-11-01 14:06:31 +0200

    SquareOfStars

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿var n = int.Parse(Console.ReadLine());
+Console.WriteLine(new String('*', n));
+
+    for (int i = 1; i <= n - 2; i++)
+{ 
+        Console.Write("*");
+        Console.Write(new String(' ', n - 2));
+        Console.WriteLine("*");
+}
+Console.WriteLine(new String('*', n));
