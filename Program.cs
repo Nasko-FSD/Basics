a12b6227119be2780568e70dc3454ff484b0e993
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2022-11-03 00:12:54 +0200

    Greeting

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter your name: ");
+var firstname = Console.ReadLine();
+Console.Write("Enter your last name: ");
+var lastname = Console.ReadLine();
+Console.Write("Enter your age: ");
+var age = Console.ReadLine();
+Console.Write("Enter your town: ");
+var town = Console.ReadLine();
+Console.WriteLine("Hello, you are {0} {1}, a {2} old person from {3}!", firstname, lastname, age, town );
