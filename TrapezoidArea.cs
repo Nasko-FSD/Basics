533683d3ce178093631b60f10f6a6976223e611a
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2022-11-04 22:11:40 +0200

    TrapezoidArea

---
+++ TrapezoidArea.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter the base of Trapezoid: ");
+var a = double.Parse(Console.ReadLine());
+Console.Write("Enter the base of Trapezoid: ");
+var b = double.Parse(Console.ReadLine());
+Console.Write("Enter the height of Trapezoid: ");
+var h = double.Parse(Console.ReadLine());
+var sum = (a + b) * h / 2.0;
+Console.Write("Trapezoid Area is: " + sum);
