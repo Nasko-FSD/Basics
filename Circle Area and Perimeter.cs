ab1cf2e23c1f9d3a8ef0776b2c976a5a87eca473
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2022-11-16 17:50:39 +0200

    Circle Area and Perimeter

---
+++ Circle Area and Perimeter.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter circle radius. r = ");
+var radius = double.Parse(Console.ReadLine());
+var area = Math.PI * radius * radius;
+var perimeter = 2 * Math.PI * radius; 
+Console.WriteLine("Circle Area = " + area);
+Console.WriteLine("Circle Perimeter = " + perimeter);
