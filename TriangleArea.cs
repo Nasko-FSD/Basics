fb00bed524e53a0f9d60475f56360c16f9e1646b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2022-11-18 17:29:58 +0200

    TriangleArea

---
+++ TriangleArea.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter side A of the triangle: ");
+var a = double.Parse(Console.ReadLine());
+Console.Write("Enter the Height of the triangle: ");
+var h = double.Parse(Console.ReadLine());
+var sum = a * h / 2;
+Console.WriteLine("Triangle Area is: " + sum);
