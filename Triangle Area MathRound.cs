a42b3112cab8f2b53573fa1491d129c038f373e3
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-01-26 23:06:53 +0200

    Triangle Area MathRound

---
+++ Triangle Area MathRound.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter Side of the Triangle: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Enter the Height of the Triangle: ");
+double h = double.Parse(Console.ReadLine());
+double area = a * h / 2;
+Console.WriteLine("Triangle area is: " + Math.Round(area, 2));
