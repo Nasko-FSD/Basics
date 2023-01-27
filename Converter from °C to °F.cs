8663e8b43a894869a290a94892e7e4feb1eaf22f
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-01-27 13:11:46 +0200

    Converter from °C to °F

---
+++ Converter from °C to °F.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter temperature °C: ");
+double C = double.Parse(Console.ReadLine());
+double sum = (C * 9/5) + 32;
+Console.WriteLine("Temperature in °F is: " + Math.Round(sum, 2));
