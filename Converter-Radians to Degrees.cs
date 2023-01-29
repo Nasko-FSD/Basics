b19e458388fbc4194f474c5f2f43531e96604a3d
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-01-29 22:22:16 +0200

    Converter-Radians to Degrees

---
+++ Converter-Radians to Degrees.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter the angle in Radians: ");
+double radians = double.Parse(Console.ReadLine());
+double sum = radians * 180 / Math.PI;
+Console.WriteLine("The angle in Degrees is: " + Math.Round(sum));
