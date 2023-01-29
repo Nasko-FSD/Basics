e7a3d9f724dde519c684833406b9810f24f20da3
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-01-29 23:51:13 +0200

    Converter- USD to BGN

---
+++ Converter- USD to BGN.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter the amount in USD: ");
+double USD = double.Parse(Console.ReadLine());
+double sum = USD * 1.79549;
+Console.WriteLine("The amount in BGN is: " + Math.Round(sum, 2));
