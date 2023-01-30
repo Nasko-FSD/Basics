992b160a3417e17b21afb647673b7420cd7d2446
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-01-30 21:15:07 +0200

    Date Calulations - 1000 Days On Earth

---
+++ Date Calulations - 1000 Days On Earth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+class _1000DaysAfterBirth
+{
+    static void Main()
+    {
+        Console.Write("Enter Bithday date: ");
+        Console.WriteLine(DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy",
+            CultureInfo.InvariantCulture).AddDays(999).ToString("dd-MM-yyyy")); 
+    }
+}
+
