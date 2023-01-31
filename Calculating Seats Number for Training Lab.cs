13e82637a69732134a9c2be8c840947900a04ca7
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-01-31 18:29:42 +0200

    Calculating Seats Number for Training Lab

---
+++ Calculating Seats Number for Training Lab.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter the length on lab: ");
+double length = double.Parse(Console.ReadLine());
+Console.Write("Enter the width on lab: ");
+double width = double.Parse(Console.ReadLine());
+double sumwidth = Math.Truncate((width - 1) / 0.7);
+double sumlength = Math.Truncate(length / 1.2);
+double seats = (sumwidth * sumlength)-3;
+Console.WriteLine("The number of seats are: " + seats);
