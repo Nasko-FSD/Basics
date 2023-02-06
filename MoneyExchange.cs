635f80674fe5543e1cb20b199eb814731f06eb0a
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-02-06 20:29:25 +0200

    MoneyExchange

---
+++ MoneyExchange.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter the amount of Bitcoin: ");
+double bitcoin = double.Parse(Console.ReadLine());
+Console.Write("Enter the amount of Chinese Yuan: ");
+double cny = double.Parse(Console.ReadLine());
+Console.Write("Enter the amount of Commission: ");
+double commission = double.Parse(Console.ReadLine()) / 100.00;
+double euro = (bitcoin * 1168 + cny * 0.15 * 1.76) / 1.95;
+double eurocommission = euro - (commission * euro);
+Console.WriteLine("The amount in Euro is: " + eurocommission);
