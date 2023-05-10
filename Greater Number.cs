01c08e639809e8d1bd4f3c7290509dfe0e637074
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-05-10 23:20:07 +0300

    Greater Number

---
+++ Greater Number.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter two integers: ");
+var num1 = double.Parse(Console.ReadLine());
+var num2 = double.Parse(Console.ReadLine());
+if ( num1 > num2)
+{
+    Console.WriteLine("Greater number: " + num1);
+}    
+else
+{
+    Console.WriteLine("Greater number: " + num2);
+}
