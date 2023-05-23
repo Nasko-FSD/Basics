9d9aa5464eec67dc14d2daaff57a540b0f21da82
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-05-23 20:42:43 +0300

    Bonus Score

---
+++ Bonus Score.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Enter score: ");
+var num = double.Parse(Console.ReadLine());
+var bonus = 0.0;
+
+if (num <= 100)
+{
+    bonus = bonus + 5;
+}
+else if (num <= 1000)
+{
+    bonus = bonus + num * 0.2;
+}
+else if (num > 1000)
+        {
+    bonus = bonus + num * 0.1;
+}
+
+if (num % 2 == 0)
+{
+    bonus = bonus + 1;
+}
+else if (num % 10 == 5)
+{
+    bonus = bonus + 2;
+}
+Console.WriteLine("Bonus score: " + bonus);
+Console.WriteLine("Total score: {0}",num + bonus);
