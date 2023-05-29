05a7eef52d6b2733c581a64d2dbaa29b16c53e44
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-05-29 17:15:00 +0300

    Sum Seconds

---
+++ Sum Seconds.cs
@@ -0,0 +1,16 @@
+﻿var num = int.Parse(Console.ReadLine());
+var num1 =int.Parse(Console.ReadLine());
+var num2 =int.Parse(Console.ReadLine());
+var seconds = num + num1 + num2;
+var minutes = seconds / 60;
+seconds = seconds % 60;
+
+if (seconds < 10)
+{
+    Console.WriteLine(minutes + ":0" + seconds);
+
+}
+else
+{
+    Console.WriteLine(minutes + ":" + seconds);
+}
