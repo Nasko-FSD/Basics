e032aa5f3fdbc606f2f51d447b164c16ecb274aa
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-06-08 18:51:40 +0300

    Password Guess

---
+++ Password Guess.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter the password: ");
+var password = (Console.ReadLine());
+string realpassword = "s3cr3t!P@ssw0rd";
+if ( password != realpassword)
+{
+    Console.Write("Wrong password!");
+}
+else if ( password == realpassword)
+{
+    Console.WriteLine("Welcome");
+}
+
+
