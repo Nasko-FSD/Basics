3ae82d08c9429d47bfbdca03558f82b03456e590
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-06-11 23:21:23 +0300

    Identical Words

---
+++ Identical Words.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your first word: ");
+string word1 = (Console.ReadLine());
+Console.Write("Enter your second word: ");
+string word2 = (Console.ReadLine());
+if (word1.ToLower() == word2.ToLower())
+{
+    Console.WriteLine("yes");
+}
+else if (word1 != word2)
+        {
+    Console.WriteLine("no");
+}
