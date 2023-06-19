129cea3272f39c3a7c2fd8cb9231f56e04f1cfaf
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-06-19 16:08:45 +0300

    3 Equal Numbers

---
+++ 3 Equal Numbers.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter your first number: ");
+var number1 = int.Parse(Console.ReadLine());
+Console.Write("Enter your second number: ");
+var number2 = int.Parse(Console.ReadLine());
+Console.Write("Enter your third number: ");
+var number3 = int.Parse(Console.ReadLine());
+if (number1 == number2 && number2 == number3)
+{
+    Console.WriteLine("yes");
+}
+if (number1 != number2 && number1 != number3)
+{
+    Console.WriteLine("no");
+}
+else if ( number2 != number3)
+{
+    Console.WriteLine("no");
+}
