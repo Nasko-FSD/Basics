9ec5b46daf464dfe2bdf81748d9c396512acdc2b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-06-19 14:38:54 +0300

    Update Time + 15 Minutes.cs

--- Time + 15 Minutes.cs
+++ Time + 15 Minutes.cs
@@ -1,26 +1,22 @@
-﻿Console.Write("Enter hours:");
+Console.Write("Enter hours:");
 double hours = double.Parse(Console.ReadLine());
 Console.Write("Enter minutes:");
 double minutes = double.Parse(Console.ReadLine());
 minutes = minutes + 15;
 if (minutes >= 60)
 {
-minutes = minutes - 60;
-hours++;
+    minutes = minutes % 60;
+    hours++;
 }
-else if (hours > 23)
+if (hours >= 24)
 {
-hours = hours - 24;
-}
-if (hours == 24)
-{
-hours = hours - 24;
+    hours = hours % 24;
 }
 if (minutes < 10)
 {
-Console.WriteLine(hours + ":0" + minutes);
+    Console.WriteLine("{0}:0{1}", hours, minutes);
 }
 else if (minutes >= 10)
 {
-Console.WriteLine(hours + ":" + minutes);
+    Console.WriteLine("{0}:{1}", hours, minutes);
 }
