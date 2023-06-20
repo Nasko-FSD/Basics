0837100d1d7ec6ec0e246152eaac1639e5668bd8
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-06-20 19:16:24 +0300

    Transportation Price

---
+++ Transportation Price.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Enter the kilometers: ");
+int kilometers = int.Parse(Console.ReadLine());
+Console.Write("Enter the period - day or night: ");
+string period = Console.ReadLine();
+double price = 0;
+double taxistartingfee = 0.70;
+double taxidayrate = 0.79;
+double taxinightrate = 0.90;
+double bus = 0.09;
+double train = 0.06;
+if (kilometers < 20 && period == "day")
+{
+    price = taxistartingfee + (kilometers * taxidayrate);
+}
+else if ( kilometers < 20 && period == "night")
+{
+    price = taxistartingfee + (kilometers * taxinightrate);
+}
+if (kilometers >= 20 && kilometers < 100)
+{
+    price = kilometers * bus;
+}
+else if (kilometers >= 100 && kilometers <= 5000)
+{
+    price = kilometers * train;
+}
+Console.WriteLine("The price is: " + Math.Round(price, 2));
