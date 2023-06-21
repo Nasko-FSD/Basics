f9e64a85fe58b0a9224b57cfd1cfb92de4d2b6e7
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-06-21 16:02:15 +0300

    Sleepy Tom Cat

---
+++ Sleepy Tom Cat.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter the number of holidays: ");
+var holidays = int.Parse(Console.ReadLine());
+var workingdays = 365 - holidays;
+var totalPlayMinutes = workingdays * 63 + holidays * 127;
+var difference = Math.Abs(totalPlayMinutes - 30000);
+var hours = difference / 60;
+var minutes = difference % 60;
+if (totalPlayMinutes > 30000)
+{
+    Console.WriteLine("Tom will run away");
+    Console.WriteLine("{0} hours and {1} minutes more for play", hours, minutes);
+}
+else
+{
+    Console.WriteLine("Tom sleeps well");
+    Console.WriteLine("{0} hours and {1} minutes less for play", hours, minutes);
+}
