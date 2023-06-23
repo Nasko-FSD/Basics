befa3f7d3e9f85c789daa971c160495d30758d28
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-06-23 18:49:18 +0300

    Personal Titles

---
+++ Personal Titles.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter the age on person: ");
+var age = double.Parse(Console.ReadLine());
+Console.Write("Enter the sex on person: ");
+string gender = (Console.ReadLine());
+if (gender == "m" && age >= 16)
+{
+    Console.WriteLine("Mr.");
+}
+else if (gender == "m" && age < 16)
+{
+    Console.WriteLine("Master");
+}
+if (gender == "f" && age >= 16)
+{
+    Console.WriteLine("Ms.");
+}
+else if (gender == "f" && age < 16)
+{
+    Console.WriteLine("Miss");
+}
