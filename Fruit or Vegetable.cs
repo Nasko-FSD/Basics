d7dc6e7baf2499061baab298417d337fe88f987a
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-07-06 22:17:32 +0300

    Fruit or Vegetable

---
+++ Fruit or Vegetable.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter what you want to buy: ");
+string word = (Console.ReadLine());
+if (word == "banana" || word == "apple" || word == "kiwi" || word == "cherry" || word == "lemon" || word == "grapes")
+{
+    Console.WriteLine("fruit");
+}
+else if (word == "tomato" || word == "cucumber" || word == "pepper" || word == "carrot")
+{
+    Console.WriteLine("vegetable");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
