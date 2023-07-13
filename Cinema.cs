3c9a573184a2b1ac88c054307b6d97227a8ea8d5
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-07-13 21:13:44 +0300

    Cinema

---
+++ Cinema.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter the of projection: ");
+var projection = (Console.ReadLine()).ToLower();
+Console.Write("Enter the number of rows: ");
+var rows = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+var columns = int.Parse(Console.ReadLine());
+double premiere = 12;
+double normal = 7.50;
+double discount = 5;
+if (projection == "premiere")
+{
+    Console.WriteLine("{0:f2}", premiere * (rows * columns));
+}
+else if (projection == "normal")
+{
+    Console.WriteLine("{0:f2}", normal * (rows * columns));
+}
+else if (projection == "discount")
+{
+    Console.WriteLine("{0:f2}", discount * (rows * columns));
+}
