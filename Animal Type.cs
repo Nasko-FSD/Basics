c3b37413b608340e2a148815b2a8d8ddc48e087c
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-07-13 20:19:17 +0300

    Animal Type

---
+++ Animal Type.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter an animal: ");
+var animal = (Console.ReadLine());
+if ( animal == "tortoise" || animal == "snake" || animal == "crocodile")
+{
+    Console.WriteLine("reptile");
+}
+else if (animal == "dog")
+{
+    Console.WriteLine("mammal");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
