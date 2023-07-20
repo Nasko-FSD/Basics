d0a9d1f2ccfe844a27a9aab9fc3937a954fb9e5d
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-07-20 16:29:39 +0300

    Trip

---
+++ TRIP.cs
@@ -0,0 +1,49 @@
+﻿Console.Write("Enter the budget: ");
+double budget = double.Parse(Console.ReadLine());
+Console.Write("Enter the season - summer or winter: ");
+string season = (Console.ReadLine());
+if (budget <= 100)
+{
+    if (season == "summer")
+    {
+        budget = budget * 30 / 100;
+        Console.WriteLine("Somewhere in Bulgaria");
+        Console.WriteLine("Camp - {0:f2}", budget);
+    }
+    else if (season == "winter")
+    {
+        budget = budget * 70 / 100;
+        Console.WriteLine("Somewhere in Bulgaria");
+        Console.WriteLine("Hotel - {0:f2}", budget);
+    }
+}
+if (budget > 100 && budget <= 1000)
+{
+    if (season == "summer")
+    {
+        budget = budget * 40 / 100;
+        Console.WriteLine("Somewhere in Balkans");
+        Console.WriteLine("Camp - {0:f2}", budget);
+    }
+    else if (season == "winter")
+    {
+        budget = budget * 80 / 100;
+        Console.WriteLine("Somewhere in Balkans");
+        Console.WriteLine("Hotel - {0:f2}", budget);
+    }
+}
+if ( budget > 1000 )
+{
+    if (season == "summer")
+    {
+        budget = budget * 90 / 100;
+        Console.WriteLine("Somewhere in Europe");
+        Console.WriteLine("Hotel - {0:f2}", budget);
+    }
+    else if (season == "winter")
+    {
+        budget = budget * 90 / 100;
+        Console.WriteLine("Somewhere in Europe");
+        Console.WriteLine("Hotel - {0:f2}", budget);
+    }
+}
