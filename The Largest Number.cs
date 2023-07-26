b4fff7de19fb99ca5f2e981a41883e98c693922f
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-07-26 18:52:49 +0300

    The Largest Number

---
+++ The Largest Number.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter how many numbers you'r gonna enter: ");
+var n = int.Parse(Console.ReadLine());
+Console.Write("Enter {0} numbers: ", n);
+var max = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n-1; i++)
+{
+    Console.Write("nume {0}= ", i);
+    var num = int.Parse(Console.ReadLine());
+    if (num > max)
+    {
+        max = num;
+    }
+}
+Console.WriteLine("The largest number is: " + max);
