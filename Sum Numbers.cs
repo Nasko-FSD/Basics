d0d6a48b56b71aa75b8260629f80c2b24667b329
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-07-26 17:03:57 +0300

    Sum Numbers

---
+++ Sum Numbers.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter how many numbers you'r gonna sum: ");
+var n = int.Parse(Console.ReadLine());
+Console.Write("Enter {0} numbers: ", n);
+var sum = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write("num {0} = ", i);
+    var num = int.Parse(Console.ReadLine());
+    sum = sum + num;
+}
+Console.Write("The sum is: " + sum);
