91b8bebe600c619f1c50f95a36101df713eca14d
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-07-27 17:04:57 +0300

    The Smallest Number

---
+++ The Smallest Number.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter how many numbers you'r gonna enter: ");
+var n = int.Parse(Console.ReadLine());
+Console.Write("Enter {0} numbers: ", n);
+var min = int.MaxValue;
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("num {0}= ", i);
+    var num = int.Parse(Console.ReadLine());
+    if (num < min)
+    {
+        min = num;
+    }
+}
+Console.WriteLine("The Smallest number is: " + min);
