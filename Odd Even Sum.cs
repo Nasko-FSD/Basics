e7d794632eaba525250ecc3343ec15c3e67702d8
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-02 16:55:25 +0300

    Odd Even Sum

---
+++ Odd Even Sum.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Enter how many numbers you'r gonna enter: ");
+var n = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter {0} numbers: ", n);
+var oddsum = 0;
+var evensum = 0;
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("num {0}= ", i);
+    var num = int.Parse(Console.ReadLine());
+    if (i % 2 == 0)
+    {
+    evensum = evensum + num;
+    }
+    else 
+    {
+    oddsum = oddsum + num;
+    }
+}
+if (oddsum == evensum)
+{
+    Console.WriteLine("Yes, sum = " + oddsum);
+}
+else 
+{
+    int diff = Math.Abs(evensum - oddsum);
+    Console.WriteLine("No, diff = " + diff);
+}
