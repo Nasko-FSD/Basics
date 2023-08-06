1a08c51cc8085caf5ff89a683432eda61b1fce60
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-06 20:56:05 +0300

    Rectangle Of N x N Stars

---
+++ Rectangle Of N x N Stars.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter rectangle from how many stars you want to draw: ");
+var n = int.Parse(Console.ReadLine());
+for ( int row = 1; row <= n; row++)
+{
+    for (int col = 1; col <= n; col++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+} 
