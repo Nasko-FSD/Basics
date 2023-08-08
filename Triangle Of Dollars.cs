389396006db22d13b9a16b114fc2dba25235309f
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-08 19:05:54 +0300

    Triangle Of Dollars

---
+++ Triangle Of Dollars.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a triangle of how many $ you want to draw: ");
+var n = int.Parse(Console.ReadLine());
+for (int row = 1; row <= n; row++)
+{
+    Console.Write("$");
+    for (int col = 1; col < row; col++)
+    {
+        Console.Write(" $");
+    }
+    Console.WriteLine();
+}
