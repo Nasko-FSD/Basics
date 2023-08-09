ebda1f4110b04f74de73278f5a70377f6ccb6258
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-09 22:38:49 +0300

    Square Frame

---
+++ Square Frame.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Enter a Square Frame from how many n*n signs you want to draw: ");
+var n = int.Parse(Console.ReadLine());
+Console.Write("+");
+for (int row = 1; row < n - 1; row++)
+{
+    Console.Write(" -");
+}
+Console.WriteLine(" +");
+for (int col = 1; col < n - 1; col++)
+{
+    Console.Write("|");
+    for (int row = 1; row < n - 1; row++)
+    {
+        Console.Write(" -");
+    }
+    Console.WriteLine(" |");
+}
+Console.Write("+");
+for (int row = 1; row < n - 1; row++)
+{
+    Console.Write(" -");
+}
+Console.Write(" +");
