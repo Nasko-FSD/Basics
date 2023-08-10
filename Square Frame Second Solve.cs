79fa5268773cbe1ba7a780f02f86c413eea394da
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-10 13:47:47 +0300

    Square Frame Second Solution

---
+++ Square Frame Second Solve.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Enter a Square From from how many n*n symbols you want to draw: ");
+var n = int.Parse(Console.ReadLine());
+for (int row = 0; row < n; row++)
+{
+    if(row == 0 || row == n - 1)
+    {
+        Console.Write("+");
+    }
+    else
+    {
+        Console.Write("|");
+    }
+    for (int col = 0; col < n - 2; col++)        
+    {
+            Console.Write(" -");
+    }
+    if (row == 0 || row == n - 1)
+    {
+        Console.Write(" +");
+    }
+    else
+    {
+        Console.Write(" |");
+    } 
+    Console.WriteLine();
+}
