799adcdb7b7aabc787c5e6cbee2f91593566a933
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-15 14:26:50 +0300

    Christmas Three My First Solve

---
+++ Christmas Three My First Solve.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter a number from how many stars you want to draw a Christmas Three: ");
+var n = int.Parse(Console.ReadLine());
+for (int row = 0; row <= n; row++)
+{
+    for (int col = 1; col <= n - row; col++)
+    {
+        Console.Write(" ");
+    }
+    for (int col = 1; col <= row; col++)
+    {
+        Console.Write("*");
+    }
+        Console.Write(" | ");
+    for (int col = 1; col <= row; col++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
