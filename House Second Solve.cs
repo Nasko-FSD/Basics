9c348c6595f7b9f917f33c15165c358112df6e35
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-18 16:27:45 +0300

    House Second Solve

---
+++ House Second Solve.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace House
+{
+    class House
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("On how many floors do you want to draw a house: ");
+            var n = int.Parse(Console.ReadLine());
+            var stars = 1;
+            if (n % 2 == 0)
+            {
+                stars = 2;
+            }
+            for (int row = 0; row < (n + 1) / 2; row++)
+            {
+                var hyphens = (n - stars) / 2;
+                Console.Write(new string('-', hyphens));
+                Console.Write(new string('*', stars));
+                Console.WriteLine(new string('-', hyphens));
+                stars += + 2;
+            }
+            for (int row = 0; row < n / 2; row++)
+            {
+                Console.Write("|");
+                Console.Write(new string('*', n - 2));
+                Console.WriteLine("|");
+            }
+            stars++;
+            }
+        }
+    }
