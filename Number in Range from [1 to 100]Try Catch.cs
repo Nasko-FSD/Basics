2c4a4c869a84dd6e5999284b026910dbee441f82
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-23 18:49:30 +0300

    Number in Range [1 to 100] Try Catch

---
+++ Number in Range from [1 to 100]Try Catch.cs
@@ -0,0 +1,18 @@
+﻿var n = -1;
+while (true)
+{
+    try
+    {
+        n = int.Parse(Console.ReadLine());
+        if (n >= 1 && n <= 100)
+        {
+            break;
+        }
+        Console.WriteLine("Invalid Number!");
+    }
+    catch
+    {
+        Console.WriteLine("This is not a number!");
+    }
+}
+Console.WriteLine("The number is: " + n);
