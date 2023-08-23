9bd4d8b2ef98d6aded87c1e602a9421640f56ab6
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-23 18:35:01 +0300

    Number in Range [1 to 100]

---
+++ Number in Range from [1 to 100[.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter a number in range from 1 to 100: ");
+var number = int.Parse(Console.ReadLine());
+while (number < 1 || number > 100)
+{
+    Console.WriteLine("Invalid number!");
+    number = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("The number is: {0}", number);
