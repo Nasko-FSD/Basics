bee456d6c781250c3ababdfd61e3324773ba4fb9
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-24 21:39:00 +0300

    Greatest Common Divisor Euclid

---
+++ Greatest Common Divisor Euclid .cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter first number: ");
+var a = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+var b = int.Parse(Console.ReadLine());
+while (b != 0)
+{
+    var oldB = b;
+    b = a % b;
+    a = oldB;
+}
+Console.WriteLine("The GCD is: {0}", a);
