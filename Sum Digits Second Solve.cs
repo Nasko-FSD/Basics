e889f78bcbe97d4c6093cd2744e6522e380681e5
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-28 22:43:56 +0300

    Sum DIgits Second Solve

---
+++ Sum Digits Second Solve.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter the number which digits you want to sum: ");
+var n = int.Parse(Console.ReadLine());
+n = Math.Abs(n);
+var sum = 0;
+while (n > 0)
+{
+    var digit = n % 10;
+    sum = sum + digit;
+    n = n / 10;
+}
+Console.WriteLine("The sum of digits is: {0}", sum);
