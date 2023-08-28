a2c412026e4e48977021453c988f0b7a97160ca0
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-28 22:38:19 +0300

    Sum Digits First Solve

---
+++ Sum Digits First Solve.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter the number which digits you want to sum: ");
+var n = int.Parse(Console.ReadLine());
+n = Math.Abs(n);
+var sum = 0;
+do
+{
+    sum = sum + n % 10;
+    n = n / 10;
+} while (n > 0);
+Console.WriteLine("The sum of digits is: {0}", sum);
