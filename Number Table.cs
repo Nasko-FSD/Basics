ffef10ded167735fe056e20ba037ff5446cd8aa3
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-30 22:07:07 +0300

    Number Table

---
+++ Number Table.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter a number: ");
+var n = int.Parse(Console.ReadLine());
+for (int row = 0; row < n; row++)
+{
+    for (int col = 1; col <= n; col++)
+    {
+        var num = (col + row);
+        if (num > n)
+        {
+            num = -num + 2 * n;
+        }
+        if (col > 1)
+        {
+            Console.Write(" ");
+        }
+        Console.Write(num);        
+    }
+    Console.WriteLine();
+}
