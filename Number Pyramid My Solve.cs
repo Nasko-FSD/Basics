6f316a7b25838586b55e90bddc1c14566119eaa0
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-30 21:34:28 +0300

    Number Pyramid My Solve

---
+++ Number Pyramid My Solve.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter a number: ");
+var n = int.Parse(Console.ReadLine());
+var num = 1;
+for (int row = 1; row <= n; row++)
+{
+    for (int col = 1; col <= row; col++)
+    {
+        if (col > 1)
+        {
+            Console.Write(" ");
+        }
+        if (num >= n + 1)
+        {
+            break;
+        }
+        Console.Write(num);
+        num++;
+    }
+    Console.WriteLine();
+}
