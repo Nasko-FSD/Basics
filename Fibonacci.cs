699823a264ce195ee2e13d8081a558cc18f6fdd2
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-30 20:37:32 +0300

    Fibonacci sequence

---
+++ Fibonacci.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a number to check Fibonacci's sequence: ");
+var n = int.Parse(Console.ReadLine());
+var fibonacci0 = 1;
+var fibonacci1 = 1;
+for ( int i = 1; i <= n - 1; i++)
+{
+    var FibonacciNext = fibonacci0 + fibonacci1;
+    fibonacci0 = fibonacci1;
+    fibonacci1 = FibonacciNext;
+}
+Console.WriteLine("Fibonacci's sequence: {0}" , fibonacci1);
