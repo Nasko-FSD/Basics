85f4b17d305dcdb047597285bc4bdf420b18e52a
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-08-30 17:01:53 +0300

    Enter Even Number

---
+++ Enter Even Number.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter Even number: ");
+var n = 0;
+while (true)
+{
+    try
+    {
+        n = int.Parse(Console.ReadLine());
+        if (n % 2 == 0)
+        {
+            break;
+        }
+        Console.WriteLine("The number is not even.");
+    }
+    catch
+    {
+        Console.WriteLine("This is not a number");
+    }
+}
+Console.WriteLine("Even number entered: {0}", n);
