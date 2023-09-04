127b6146e94da1a4ff3d5ba17e878ac215e26877
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-09-04 15:10:34 +0300

    Stop Number

---
+++ Stop Number.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter a number N: ");
+var N = int.Parse(Console.ReadLine());
+Console.Write("Enter a number M: ");
+var M = int.Parse(Console.ReadLine());
+Console.Write("Enter a number Stop: ");
+var Stop = int.Parse(Console.ReadLine());
+for (int i = M; i >= N; i--)
+{
+    if (i % 2 == 0 && i % 3 == 0 && i == Stop)
+    {
+        break;
+    }
+    else if (i % 2 == 0 && i % 3 == 0 && i != Stop)
+    {
+        Console.Write("{0} ", i);
+    }
+}
