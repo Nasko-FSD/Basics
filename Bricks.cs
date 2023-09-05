082dad1b28db1f22eecdcc13a73da9ddaff39780
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-09-05 16:31:38 +0300

    Bricks

---
+++ Bricks.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter the number of bricks: ");
+double x = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of workers: ");
+double w = int.Parse(Console.ReadLine());
+Console.Write("Enter the volume of the trunk: ");
+double m = int.Parse(Console.ReadLine());
+double calculations = (x / (w * m));
+Console.WriteLine("The amount of courses needed are:{0}", Math.Ceiling(calculations));
