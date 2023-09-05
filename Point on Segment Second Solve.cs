a78d4211e6c3945733e686d7ba0bda498e9c2ca2
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-09-05 20:56:09 +0300

    Point on Segment Second Solve

---
+++ Point on Segment Second Solve.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter first number: ");
+var firstEnd = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+var secondEnd = int.Parse(Console.ReadLine());
+Console.Write("Enter the point: ");
+var point = int.Parse(Console.ReadLine());
+var left = Math.Min(firstEnd, secondEnd);
+var right = Math.Max(firstEnd, secondEnd);
+var pointOnSegment = (point >= left && point <= right);
+var leftDistance = Math.Abs(point - left);
+var rightDistance = Math.Abs(point - right);
+var distance = Math.Min(leftDistance, rightDistance);
+if (pointOnSegment)
+{
+    Console.WriteLine("in");
+}
+else
+{
+    Console.WriteLine("out");
+}
+Console.WriteLine(distance);
