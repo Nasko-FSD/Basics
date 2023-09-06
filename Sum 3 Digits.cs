bd1945d5dddae7866c0b61eea05932405354e4b4
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-09-06 21:59:26 +0300

    Sum 3 Numbers

---
+++ Sum 3 Digits.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter 3 numbers: ");
+var num1 = int.Parse(Console.ReadLine());
+var num2 = int.Parse(Console.ReadLine());
+var num3 = int.Parse(Console.ReadLine());
+var min  = Math.Min(Math.Min(num1, num2), num3);
+var max = Math.Max(Math.Max(num1, num2), num3);
+var sum = num1 + num2 + num3;
+var mid = sum - min - max;
+if (min + mid == max)
+{
+    Console.WriteLine("{0} + {1} = {2}", min, mid, max);
+}
+else
+{
+    Console.WriteLine("No");
+}
