dcf9658b1c8222807be7ad29b87298b125496de0
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-09-06 20:32:38 +0300

    Date After 5 Days

---
+++ Date After 5 Days.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Enter the day from 1 to 7: ");
+var day = int.Parse(Console.ReadLine());
+Console.Write("Enter the month from 01 to 12: ");
+var month = int.Parse(Console.ReadLine());
+var daysinTheMonth = 31;
+if (month == 04 || month == 06 || month == 09 || month == 11)
+{
+    daysinTheMonth = 30;
+}
+else if (month == 02)
+{
+    daysinTheMonth = 28;
+}
+day = day + 5;
+if (day > daysinTheMonth)
+{
+    day = day - daysinTheMonth;
+    month++;
+}
+if (month > 12)
+{
+    month = 01;
+}
+if (month < 10)
+{
+    Console.WriteLine("{0}.0{1}", day, month);
+}
+else
+{
+    Console.WriteLine("{0}.{1}", day, month);
+}
