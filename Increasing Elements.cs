224a096151b5700651fce1027337a69456bab9dc
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-09-08 23:51:29 +0300

    Increasing Elements

---
+++ Increasing Elements.cs
@@ -0,0 +1,26 @@
+﻿var n = int.Parse(Console.ReadLine());
+var CurrentLenght = 0;
+var maxLenght = 0;
+var previousNumber = int.MinValue;
+for (int i = 0; i < n; i++)
+{
+    var CurrentNumber = int.Parse(Console.ReadLine());
+    if (CurrentNumber > previousNumber)
+    {
+        CurrentLenght++;
+    }
+    else
+    {
+        if (maxLenght < CurrentLenght)
+        {
+            maxLenght = CurrentLenght;
+        }
+        CurrentLenght = 1;
+    }
+    previousNumber = CurrentNumber;
+}
+if (maxLenght < CurrentLenght)
+{
+    maxLenght = CurrentLenght;
+}
+Console.WriteLine(maxLenght);
