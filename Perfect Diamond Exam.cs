03dec6cf82249fcf1853886f21bef07e13191c33
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-09-13 23:08:18 +0300

    Perfect Diamond Preparing For Exam

---
+++ Perfect Diamond Exam.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter a number to draw a perfect diamond: ");
+var n = int.Parse(Console.ReadLine());
+for (int row = 1; row <= n; row++)
+{
+    Console.Write(new string(' ', n - row));
+    Console.Write("*");
+    for (int col = 1; col < row; col++)
+    {
+        Console.Write("-*");
+    }
+    Console.WriteLine();
+}
+for (int row = 1; row < n; row++)
+{
+    Console.Write(new string(' ', row));
+    Console.Write("*");
+    for (int col = 2; col <= n - row; col++)
+    {
+        Console.Write("-*");
+    }
+    Console.WriteLine();
+}
