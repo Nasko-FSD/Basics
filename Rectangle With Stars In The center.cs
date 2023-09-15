8f5c1b5b2479fdfc34390d9467427f2e6cf19099
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-09-15 12:42:04 +0300

    Rectangle With Stars In The Center

---
+++ Rectangle With Stars In The center.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter a number to draw a rectangle: ");
+var n = int.Parse(Console.ReadLine());
+Console.WriteLine(new string('%', n * 2));
+int numRows = n - 1;
+if (n % 2 == 1) numRows++;
+for (int row = 0; row < numRows; row++)
+{
+    Console.Write("%");
+    Console.Write(new string(' ', n - 2));
+    if (row == (n - 1) / 2)
+    {
+        Console.Write("**");
+    }
+    else
+    {
+        Console.Write("  ");
+    }
+    Console.Write(new string(' ', n - 2));
+    Console.Write("%");
+    Console.WriteLine();
+}
+Console.WriteLine(new string('%', n * 2));
