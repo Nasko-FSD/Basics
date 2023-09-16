db719b5e63048efed5311c5e40a0116810663c6d
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-09-16 21:26:31 +0300

    Increasing 4 Numbers My Solve

---
+++ Increasing 4 Numbers My Solve.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Enter a number: ");
+var a = int.Parse(Console.ReadLine());
+Console.Write("Enter a number: ");
+var b = int.Parse(Console.ReadLine());
+if (b < a + 3)
+{
+    Console.WriteLine("No");
+}
+else
+{
+    for (int col = a; col <= b; col++)
+    {
+        for (int j = col + 1; j <= b; j++)
+        {
+            for (int k = j + 1; k <= b; k++)
+            {
+                for (int l = k + 1; l <= b; l++)
+                {
+                    Console.Write("{0} {1} {2} {3}", col, j, k, l);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
