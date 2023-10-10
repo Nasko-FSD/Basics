6581805bd9aebf2f00b477c573a9709ec3b3f11b
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-10 22:00:52 +0300

    Hospital

---
+++ Hospital.cs
@@ -0,0 +1,30 @@
+﻿var period = int.Parse(Console.ReadLine());
+var treated = 0;
+var untreated = 0;
+var doctors = 7;
+for (int day = 1; day <= period; day++)
+{
+    var patients = int.Parse(Console.ReadLine());
+    if (day % 3 == 0 && untreated > Math.Abs(treated))
+    {
+        doctors++;
+    }
+    if (doctors - patients >= 0)
+    {
+        if (doctors - patients > 0)
+        {
+            treated += patients;
+        }
+        if (doctors - patients == 0)
+        {
+            treated += doctors;
+        }
+    }
+    if (doctors - patients < 0)
+    {
+        treated += doctors;
+        untreated += Math.Abs(patients - doctors);
+    }
+}
+Console.WriteLine("Treated patients: {0}.", treated);
+Console.WriteLine("Untreated patients: {0}.", untreated);
