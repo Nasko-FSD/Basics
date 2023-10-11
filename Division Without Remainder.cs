a25168e1f4ee43def1421a855cdccd548f41a1d5
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-11 10:56:32 +0300

    Division Without Remainder

---
+++ Division Without Remainder.cs
@@ -0,0 +1,23 @@
+﻿var n = int.Parse(Console.ReadLine());
+double p1 = 0;
+double p2 = 0;
+double p3 = 0;
+for (int i = 1; i <= n; i++)
+{
+    var numbers = int.Parse(Console.ReadLine());
+    if (numbers % 2 == 0)
+    {
+        p1++;
+    }
+    if (numbers % 3 == 0)
+    {
+        p2++;
+    }
+    if (numbers % 4 == 0)
+    {
+        p3++;
+    }
+}
+Console.WriteLine("{0:f2}%", p1 / n * 100);
+Console.WriteLine("{0:f2}%", p2 / n * 100);
+Console.WriteLine("{0:f2}%", p3 / n * 100);
