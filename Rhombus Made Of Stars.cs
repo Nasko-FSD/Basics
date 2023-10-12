420613517f6e40dc2e8c79b6e80bf6452474cce5
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-12 15:58:14 +0300

    Rhombus Made Of Stars

---
+++ Rhombus Made Of Stars.cs
@@ -0,0 +1,25 @@
+﻿var n = int.Parse(Console.ReadLine());
+for (int row = 1; row <= n; row++)
+{
+    for (int spaces = 1; spaces <= n - row; spaces++)
+    {
+        Console.Write(" ");
+    }
+    for (int stars = 1; stars <= row; stars++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
+for (int row = 1; row <= n - 1; row++)
+{
+    for (int spaces = 1; spaces <= row; spaces++)
+    {
+        Console.Write(" ");
+    }
+    for (int stars = 1; stars <= n - row; stars++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
