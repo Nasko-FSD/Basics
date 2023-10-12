1f0b3c3d559443a3cd74d23077700398a305db02
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-12 18:03:58 +0300

    Sunglasses Second Solve

---
+++ Sunglasses Second Solve.cs
@@ -0,0 +1,30 @@
+﻿var n = int.Parse(Console.ReadLine());
+Console.Write(new string('*', n * 2));
+Console.Write(new string(' ', n));
+Console.WriteLine(new string('*', n * 2));
+for (int row = 1; row <= n - 2; row++)
+{
+    Console.Write('*');
+    for (int slash = 1; slash <= (n * 2) - 2; slash++)
+    {
+        Console.Write("/");
+    }
+    Console.Write('*');
+    if (row == (n - 1) / 2)
+    {
+        Console.Write(new string('|', n));
+    }
+    else
+    {
+        Console.Write(new string(' ', n));
+    }
+    Console.Write('*');
+    for (int slash = 1; slash <= (n * 2) - 2; slash++)
+    {
+        Console.Write("/");
+    }
+    Console.WriteLine('*');
+}
+Console.Write(new string('*', n * 2));
+Console.Write(new string(' ', n));
+Console.Write(new string('*', n * 2));
