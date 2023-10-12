01f847a0611fe8c825f677267a3456946378fc2d
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-12 15:47:11 +0300

    Christmas Tree Second Solve

---
+++ Christmas Tree Second Solve.cs
@@ -0,0 +1,12 @@
+﻿var n = int.Parse(Console.ReadLine());
+for (int row = 0; row <= n; row++)
+{
+    for (int spaces = 1; spaces <= n - row; spaces++)
+    {
+        Console.Write(" ");
+    }
+    Console.Write(new string('*', row));
+    Console.Write(" | ");
+    Console.Write(new string('*', row));
+    Console.WriteLine();
+}
