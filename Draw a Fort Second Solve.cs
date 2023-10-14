fb01d610cdcc14de0f5677f2ec7314283c99f1bf
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-14 19:16:52 +0300

    Draw a Fort Second Solve

---
+++ Draw a Fort Second Solve.cs
@@ -0,0 +1,10 @@
+﻿var n = int.Parse(Console.ReadLine());
+var upper = n / 2;
+var mid = 2 * n - 2 * upper - 4;
+Console.WriteLine("/{0}\\{1}/{0}\\", new string('^', upper), new string('_', mid));
+for (int body = 1; body <= n - 3; body++)
+{
+    Console.WriteLine("|{0}|", new string(' ', 2 * n - 2));
+}
+Console.WriteLine("|{0}{1}{0}|", new string(' ', upper + 1), new string('_', mid));
+Console.WriteLine("\\{0}/{1}\\{0}/", new string('_', upper), new string(' ', mid));
