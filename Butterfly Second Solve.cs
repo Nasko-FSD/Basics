eff9036359fe1f06372db57ac92ae384a576bf9d
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-14 22:59:22 +0300

    Butterfly Second Solve

---
+++ Butterfly Second Solve.cs
@@ -0,0 +1,26 @@
+﻿var n = int.Parse(Console.ReadLine());
+var stars = n - 2;
+var hyphens = n - 2;
+for (int upper = 1; upper <= n - 2; upper++)
+{
+    if (upper % 2 == 1)
+    {
+        Console.WriteLine("{0}\\ /{0}", new string('*', stars));
+    }
+    if (upper % 2 == 0)
+    {
+        Console.WriteLine("{0}\\ /{0}", new string('-', hyphens));
+    }
+}
+Console.WriteLine("{0}@{0}", new string(' ', n - 1));
+for (int upper = 1; upper <= n - 2; upper++)
+{
+    if (upper % 2 == 1)
+    {
+        Console.WriteLine("{0}/ \\{0}", new string('*', stars));
+    }
+    if (upper % 2 == 0)
+    {
+        Console.WriteLine("{0}/ \\{0}", new string('-', hyphens));
+    }
+}
