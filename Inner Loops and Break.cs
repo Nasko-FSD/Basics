80e1dced17138b1a620c83f57729563fedba4c95
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-17 18:52:56 +0300

    Inner Loops and Break

---
+++ Inner Loops and Break.cs
@@ -0,0 +1,16 @@
+﻿var hasToEnd = false;
+for (int i = 1; i <= 3; i++)
+{
+    if (hasToEnd == false)
+    {
+        for (int j = 3; j >= 1; j--)
+        {
+            if (i + j == 2)
+            {
+                hasToEnd = true;
+                break;
+            }
+            Console.WriteLine(i + " " + j);
+        }
+    }
+}
