b1fa7805133cea53386aaa0206d22dcb0eacbc5c
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-18 19:42:20 +0300

    Point On Segment Third Solve

---
+++ Point On Segment Third Solve.cs
@@ -0,0 +1,19 @@
+﻿var firstend = int.Parse(Console.ReadLine());
+var secondend = int.Parse(Console.ReadLine());
+var point = int.Parse(Console.ReadLine());
+var left = Math.Min(firstend, secondend);
+var right = Math.Max(firstend, secondend);
+if (point >= left && point <= right)
+{
+    Console.WriteLine("in");
+    var closertoleft = point - left;
+    var closertoright = right - point;
+    Console.WriteLine(Math.Min(closertoleft, closertoright));
+}
+else
+{
+    Console.WriteLine("out");
+    var closertoleft = left - point;
+    var closertoright = point - right;
+    Console.WriteLine(Math.Max(closertoleft, closertoright));
+}
