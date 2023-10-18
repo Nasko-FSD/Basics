d85879f7485507e78074ca4475c96d485e0cae38
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-18 12:45:41 +0300

    Number Table Second Solve

---
+++ Number Table Second Solve.cs
@@ -0,0 +1,18 @@
+﻿var n = int.Parse(Console.ReadLine());
+for (int row = 1; row <= n; row++)
+{
+    var num = row;
+    for (int col = 1; col <= n; col++)
+    {
+        if (num > n)
+        {
+            Console.Write("{0} ", n * 2 - num);
+        }
+        else
+        {
+            Console.Write("{0} ", num);
+        }
+        num++;
+    }
+    Console.WriteLine();
+}
