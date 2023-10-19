1abf257a691454cbab97d95641166fe7edfb7466
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-19 13:42:10 +0300

    Sequence Of Increasing Elements Second Solve

---
+++ Sequence of Increasing Elements Second Solve.cs
@@ -0,0 +1,22 @@
+﻿var n = int.Parse(Console.ReadLine());
+var previous = int.MinValue;
+var maxLenght = 0;
+var lenght = 0;
+for (int i = 0; i < n; i++)
+{
+    var CurrNumber = int.Parse(Console.ReadLine());
+    if (CurrNumber > previous)
+    {
+        lenght++;
+    }
+    else
+    {
+        lenght = 1;
+    }
+    if (maxLenght < lenght)
+    {
+        maxLenght = lenght;
+    }
+    previous = CurrNumber;
+}
+Console.WriteLine(maxLenght);
