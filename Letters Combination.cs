dc80385cc59e0e21fee95bfe361e6587690db4e0
Nasko-FSD <108883646+Nasko-FSD@users.noreply.github.com>
2023-10-22 13:24:23 +0300

    Letters Combination

---
+++ Letters Combination.cs
@@ -0,0 +1,19 @@
+﻿char First = char.Parse(Console.ReadLine().ToLower());
+char Second = char.Parse(Console.ReadLine().ToLower());
+char Third = char.Parse(Console.ReadLine().ToLower());
+var combination = 0;
+for (char one = First; one <= Second; one++)
+{
+    for (char two = First; two <= Second; two++)
+    {
+        for (char three = First; three <= Second; three++)
+        {
+            if (one != Third && two != Third && three != Third)
+            {
+                Console.Write("{0}{1}{2} ", one, two, three);
+                combination++;
+            }
+        }
+    }
+}
+Console.Write("{0} ", combination);
